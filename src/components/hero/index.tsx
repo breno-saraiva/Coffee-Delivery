5d539538db18c44eab885e5666d2e7afa50277c2
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-09-02 13:50:55 -0300

    responsive

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,47 +3,45 @@ import { ShoppingCart, Timer, Package, Coffee } from "@phosphor-icons/react";
 
 function Hero() {
   return (
-    <section className="grid  grid-cols-1 gap-14 mt-23 pt-4 sm:justify-items-end items-start sm:grid-cols-2 sm:mt-20">
-      <div className="py-4">
+    <section className="mt-20 grid grid-cols-1 justify-items-center sm:justify-items-end items-start sm:grid-cols-2 sm:mt-20">
+      <div className="py-4 flex flex-col items-center sm:items-start">
         {/* title */}
-        <div className="flex flex-col gap-3">
-          <h2 className="font-bold uppercase text-4xl">
-            Encontre o café <br /> para qualquer hora do dia
-          </h2>
-          <h6>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </h6>
-        </div>
+        <h2 className="font-bold uppercase text-4xl text-center sm:text-start">
+          Encontre o café <br /> para qualquer hora do dia
+        </h2>
+        <p className="text-center sm:text-start">
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </p>
 
         {/* opções */}
-        <div className="grid grid-cols-2 gap-5 py-8 mt-4">
-          <div className="flex gap-3">
+        <div className="grid grid-cols-1 gap-8 mt-5 sm:grid-cols-2 sm:justify-items-start sm:mt-12">
+          <div className="flex gap-3 items-center">
             <div className="bg-[#C47F17] rounded-full w-8 h-8 flex items-center justify-center">
               <ShoppingCart size={16} color="#ffffff" weight="fill" />
             </div>
             <div>Compra simples e segura</div>
           </div>
 
-          <div className="flex gap-3 items-center ">
+          <div className="flex gap-3 items-center">
             <div className="bg-[#574F4D] rounded-full w-8 h-8 flex items-center justify-center">
               <Package size={16} color="#ffffff" weight="fill" />
             </div>
-            <div>Entrega rápida e rastreada</div>
+            <div>Embalagem mantém o café intacto</div>
           </div>
 
-          <div className="flex gap-3 items-center py-5">
+          <div className="flex gap-3 items-center">
             <div className="bg-[#DBAC2C] rounded-full w-8 h-8 flex items-center justify-center">
               <Timer size={16} color="#ffffff" weight="fill" />
             </div>
             <div>Entrega rápida e rastreada</div>
           </div>
 
-          <div className="flex gap-3 items-center py-5">
+          <div className="flex gap-3 items-center">
             <div className="bg-[#8047F8] rounded-full w-8 h-8 flex items-center justify-center">
               <Coffee size={16} color="#ffffff" weight="fill" />
             </div>
-            <div>Entrega rápida e rastreada</div>
+            <div>O café chega fresquinho até você</div>
           </div>
         </div>
       </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import Hero from "../../components/hero";
 
 function Home() {
   return (
-    <div className="max-w-6xl mx-auto px-4">
+    <div className="max-w-7xl mx-auto px-2">
       <Header />
       <Hero />
     </div>
