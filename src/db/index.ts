09baebff2a5aa35cbe3c2b981c0030cf2ef19fdf
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-10-17 16:28:05 -0300

    coffee-carts

---
+++ src/db/index.ts
@@ -0,0 +1,71 @@
+export const data = [{
+    img: "/src/assets/coffee-list/coffee-expresso-tradicional.png",
+    title: "Expresso Tradicional",
+    tag: "tradicional",
+    description: "O tradicional café feito com água quente e grãos moídos"
+},{
+    img: "/src/assets/coffee-list/expresso-americano.png",
+    title: "Expresso Americano",
+    tag: "tradicional",
+    description: "Expresso diluído, menos intenso que o tradicional"
+},{
+    img: "/src/assets/coffee-list/expresso-cremoso.png",
+    title: "Expresso Cremoso",
+    tag: "tradicional",
+    description: "Café expresso tradicional com espuma cremosa"
+},{
+    img: "/src/assets/coffee-list/expresso-gelado.png",
+    title: "Expresso Gelado",
+    tag: "tradicional",
+    description: "Bebida preparada com café expresso e cubos de gelo"
+},{
+    img: "/src/assets/coffee-list/cafe-leite.png",
+    title: "Café com Leite",
+    tag: "tradicional",
+    description: "Meio a meio de expresso tradicional com leite vaporizado"
+},{
+    img: "/src/assets/coffee-list/latte.png",
+    title: "Latte",
+    tag: "tradicional",
+    description: "Uma dose de café expresso com o dobro de leite e espuma cremosa"
+},{
+    img: "/src/assets/coffee-list/capuccino.png",
+    title: "Capuccino",
+    tag: "tradicional",
+    description: "Bebida com canela feita de doses iguais de café, leite e espuma"
+},{
+    img: "/src/assets/coffee-list/macchiato.png",
+    title: "Macchiato",
+    tag: "tradicional",
+    description: "Café expresso misturado com um pouco de leite quente e espuma"
+},{
+    img: "/src/assets/coffee-list/mocaccino.png",
+    title: "Mocaccino",
+    tag: "tradicional",
+    description: "Café expresso com calda de chocolate, pouco leite e espuma"
+},{
+    img: "/src/assets/coffee-list/chocolate-quente.png",
+    title: "Chocolate Quente",
+    tag: "tradicional",
+    description: "Bebida feita com chocolate dissolvido no leite quente e café"
+},{
+    img: "/src/assets/coffee-list/cubano.png",
+    title: "Cubano",
+    tag: "tradicional",
+    description: "Drink gelado de café expresso com rum, creme de leite e hortelã"
+},{
+    img: "/src/assets/coffee-list/havaiano.png",
+    title: "Havaiano",
+    tag: "tradicional",
+    description: "Bebida adocicada preparada com café e leite de coco"
+},{
+    img: "/src/assets/coffee-list/arabe.png",
+    title: "Árabe",
+    tag: "tradicional",
+    description: "Bebida preparada com grãos de café árabe e especiarias"
+},{
+    img: "/src/assets/coffee-list/irlandes.png",
+    title: "Irlandês",
+    tag: "tradicional",
+    description: "Bebida a base de café, uísque irlandês, açúcar e chantilly"
+}]
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import CoffeeCard from "../../components/coffeeCard";
 import Header from "../../components/header";
 import Hero from "../../components/hero";
+import { data } from "../../db";
 
 function Home() {
   return (
@@ -9,12 +10,16 @@ function Home() {
       <Hero />
       <main>
         <h1 className="font-baloo text-3xl mb-14">Nossos cafés</h1>
-        <CoffeeCard
-          img="\src\assets\coffee-expresso-tradicional.png"
-          title="Expresso Tradicional"
-          tag="tradicional"
-          description="O tradicional café feito com água quente e grãos moídos"
-        />
+        <div className=" flex gap-8 flex-wrap items-center">
+          {data.map((obj) => (
+            <CoffeeCard
+              img={obj.img}
+              title={obj.title}
+              tag={obj.tag}
+              description={obj.description}
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
