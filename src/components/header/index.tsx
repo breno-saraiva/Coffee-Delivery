3899ca73873ac5321f7388985f6dafef86ca601c
sprightlytree2 <luccasdaccosta@gmail.com>
2024-04-16 00:21:12 -0300

    Testando logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,10 @@
 import { MapPin } from "@phosphor-icons/react";
+import Logoimg from "../../assets/Logo@2x.svg";
 
 function Header() {
   return (
     <header className="h-28 py-8 flex items-center justify-between">
-      <img src="\src\assets\Logo@2x.svg" />
+      <img src={Logoimg} />
       <div className="flex gap-3">
         <button className="px-2 py-2 flex gap-1 items-center rounded-md bg-[#EBE5F9] w-37 h-9 text-[#8047F8]">
           <MapPin size={22} color="#8047F8" weight="fill" /> Fortaleza,CE
