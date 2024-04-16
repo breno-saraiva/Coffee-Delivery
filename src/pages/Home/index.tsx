93a3bb649015fbd5c843683e73beeb98855fdf61
sprightlytree2 <luccasdaccosta@gmail.com>
2024-04-16 00:16:45 -0300

    Mais um teste

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,7 +25,7 @@ function Home() {
         <div className=" flex gap-8 flex-wrap items-center">
           {data?.map((obj) => (
             <CoffeeCard
-              img="../../src/assets/coffee-list/expresso-americano.png"
+              img="../../src/assets/coffee-list/expresso-americano*.png"
               title={obj.title as string}
               tag={obj.tag as string}
               description={obj.description as string}
